723cf267ad635132cedd2de3ea6871cf3153d93f
Igor Santos <igorsantoscosta@gmail.com>
2022-12-27 18:11:55 -0300

    Edit button from Backdrop Component

--- src/components/Backdrop.tsx
+++ src/components/Backdrop.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { GameContext } from "../contexts/GameContext";
 import styles from "./Backdrop.module.css";
+import { Button } from "./Button";
 import { Fireworks } from "./Fireworks";
 
 export const Backdrop = () => {
@@ -16,14 +17,14 @@ export const Backdrop = () => {
             <h2>
               Jogador {playerWinner} - "{playerWinner === 1 ? "X" : "O"}"
             </h2>
-            <button onClick={restart}>Reiniciar</button>
+            <Button onClick={restart}>Reiniciar</Button>
             <Fireworks />
           </div>
         )}
         {isGameOver && (
           <div className={styles.game_over}>
             <h1>Deu velha! :&#40;</h1>
-            <button onClick={restart}>Reiniciar</button>
+            <Button onClick={restart}>Reiniciar</Button>
           </div>
         )}
       </div>
