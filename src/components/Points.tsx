d75375b8436ace23f8e9fa8d8334da7bb49450d8
Igor Santos <igorsantoscosta@gmail.com>
2022-12-28 09:42:50 -0300

    Correcting style and points

--- src/components/Points.tsx
+++ src/components/Points.tsx
@@ -3,18 +3,18 @@ import { GameContext } from "../contexts/GameContext";
 import styles from "./Points.module.css";
 
 export const Points = () => {
-  const { points } = useContext(GameContext);
+  const { points, isAutomatic } = useContext(GameContext);
 
   return (
     <>
       <div className={styles.box}>
         <div className={styles.points}>
           <div>
-            <h1>Jogador 1 - "X"</h1>
+            <h1>{isAutomatic ? "Você" : 'Jogador 1 - "X"'}</h1>
             <h2>{points[1]}</h2>
           </div>
           <div>
-            <h1>Jogador 2 - "O"</h1>
+            <h1>{isAutomatic ? "Adversário" : 'Jogador 2 - "O"'}</h1>
             <h2>{points[2]}</h2>
           </div>
         </div>
